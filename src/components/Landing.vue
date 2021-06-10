fec65e5694bd4569f0efca2b5b835abe33238000
Civan <civan.erbay@web.de>
2021-06-10 18:34:37 +0200

    Setup

---
+++ src/components/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navigation/>
+</template>
+
+<script>
+import Navigation from './Navigation.vue'
+export default {
+  name: 'Landing',
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navigation">
+        <h1 class="navigation--headline">E_MMORTALISE</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navigation'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .navigation {
+        height: 50px;
+
+        &--headline {
+            font-size: 24px;
+        }
+    }
+</style>
