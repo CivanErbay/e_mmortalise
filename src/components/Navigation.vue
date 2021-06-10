7a2edc8252ab527fd06196a21be327910a19ba3b
Civan <civan.erbay@web.de>
2021-06-10 18:46:52 +0200

    Add burger menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="navigation">
-        <h1 class="navigation--headline">E_MMORTALISE</h1>
-        <img src="./../assets/menu.png" alt="">
-    </div>
+  <div class="navigation">
+    <h1 class="navigation--headline">E_MMORTALISE</h1>
+    <img class="burger-menu" src="./../assets/menu.png" alt="" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Navigation'
-    }
+export default {
+  name: "Navigation",
+};
 </script>
 
 <style lang="scss" scoped>
-    .navigation {
-        height: 50px;
-        
-        &--headline {
-            font-size: 24px;
-             color: #200e7e;
-        }
-    }
+.navigation {
+  height: 50px;
+
+  &--headline {
+    font-size: 24px;
+    color: #200e7e;
+  }
+
+  .burger-menu {
+    height: 25px;
+    position: absolute;
+    top: 19px;
+    right: 27px;
+  }
+}
 </style>
