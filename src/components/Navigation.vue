7131f3e7340ac0c847256dceb65e50af32b2eea1
Civan <civan.erbay@web.de>
2021-07-19 10:28:01 +0200

    Add z-index

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -105,6 +105,10 @@ export default {
   padding: 0 30px;
   border-bottom: 3px $primary-background-color solid;
 
+  &--button-wrapper {
+    z-index: 2;
+  }
+
   &--headline {
     height: 30px;
   }
