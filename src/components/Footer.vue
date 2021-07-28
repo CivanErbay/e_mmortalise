85d5e78c5d265ed409814113f3701a29ca5a0579
Paco Croket <croketproduction@gmail.com>
2021-07-28 11:53:13 +0200

    grid on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template lang="">
   <div v-if="!isMobile" class="footer">
+    <span></span>
     <p class="owner">&copy; 2021 Alina Ernesti. All Rights Reserved</p>
     <button class="btn-primary">Imprint</button>
   </div>
@@ -17,24 +18,18 @@ export default {
 .footer {
   height: 80px;
   padding: 0 60px;
-  display: flex;
-  justify-content: flex-end;
-  align-content: center;
+  display: grid;
+  grid-template-columns: 10% auto 10%;
+  justify-items: center;
   align-items: center;
 
   button {
     color: $secondary-font-color;
     background-color: $primary-background-color;
 
-    &:not(:first-child) {
-      margin-left: 30px;
+    &:not(:last-child) {
+      margin-right: 30px;
     }
-  }
-
-  .owner {
-    position: absolute;
-    left: 0;
-    right: 0;
   }
 }
 </style>
